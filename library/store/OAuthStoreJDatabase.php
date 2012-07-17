522b5b9710f270d7b33418bdef3911f7e51db3f3
Ken Erickson <bookworm.productions@gmail.com>
2012-07-17 13:04:53 -0700

    added joomla compatible SQL queries

--- library/store/OAuthStoreJDatabase.php
+++ library/store/OAuthStoreJDatabase.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once dirname(__FILE__) . '/OAuthStoreSQL.php';
+require_once dirname(__FILE__) . '/OAuthStoreJSQL.php';
 
-class OAuthStoreJDatabase extends OAuthStoreSQL
+class OAuthStoreJDatabase extends OAuthStoreJSQL
 {
   /**
    * The MySQL connection 
